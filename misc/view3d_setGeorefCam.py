9bfbcb8f5637882ba9bb180aa462f0730373ad25
domlysz <domlysz@gmail.com>
2016-08-23 10:54:18 +0200

    refactor georender cam tool

--- misc/view3d_setGeorefCam.py
+++ misc/view3d_setGeorefCam.py
@@ -25,36 +25,17 @@ from bpy.props import StringProperty, BoolProperty, EnumProperty
 from ..utils.geom import BBOX
 from ..geoscene import GeoScene
 
-def listObjects(self, context):
-	#Function used to update the objects list (obj_list) used by the dropdown box.
-	objs = [] #list containing tuples of each object
-	for index, object in enumerate(bpy.context.scene.objects): #iterate over all objects
-		if object.type == 'MESH':
-			objs.append((str(index), object.name, "Object : "+object.name)) #put each object in a tuple (key, label, tooltip) and add this to the objects list
-	return objs
-
-def listCams(self, context):
-	#Function used to update the camera list (obj_list) used by the dropdown box.
-	objs = [('NEW', 'New', 'Add new camera')] #list containing tuples of each object (key, label, tooltip)
-	for index, object in enumerate(bpy.context.scene.objects): #iterate over all objects
-		if object.type == 'CAMERA':
-			objs.append((str(index), object.name, "Camera : "+object.name)) #put each object in a tuple (key, label, tooltip) and add this to the objects list
-	return objs
-
 
 
 class SetGeorenderCam(bpy.types.Operator):
 	'''
-	Add a new georef camera or update an existing one
+	Add a new georef camera or update existing camera
 	'''
 	bl_idname = "camera.georender"
 	bl_label = "Georef cam"
-	bl_description  = "Create or update a camera to render a georeferencing map"
+	bl_description = "Create or update a camera to render a georeferencing map"
 	bl_options = {"REGISTER", "UNDO"}
 
-
-	objLst = EnumProperty(attr="obj_list", name="Object", description="Choose an object", items=listObjects)
-	camLst = EnumProperty(attr="cam_list", name="Camera", description="Choose a camera", items=listCams)
 
 	name = bpy.props.StringProperty(name = "Camera name", default="Georef cam", description="")
 	target_res = bpy.props.FloatProperty(name = "Pixel size", default=5, description="Pixel size in map units/pixel", min=0.00001)
@@ -66,76 +47,89 @@ class SetGeorenderCam(bpy.types.Operator):
 
 	def draw(self, context):
 		layout = self.layout
-		layout.prop(self, 'objLst')
-		layout.prop(self, 'camLst')
-		if self.camLst == 'NEW':
-			layout.prop(self, 'name')
+		layout.prop(self, 'name')
 		layout.prop(self, 'target_res')
 
-
-	def invoke(self, context, event):
-		scn = context.scene
-		geoscn = GeoScene(scn)
-		if geoscn.isGeoref:
-			if len(self.objLst) > 0:
-				return context.window_manager.invoke_props_dialog(self)
-			else:
-				self.report({'ERROR'}, "There isn't reference object to set camera on")
-				return {'CANCELLED'}
-		else:
-			self.report({'ERROR'}, "Scene isn't georef")
-			return {'CANCELLED'}
-
-		
+	
 
 	def execute(self, context):#every times operator redo options are modified
 
-		scn = context.scene
-
-		#general offset used to set cam z loc and clip end distance
-		#needed to avoid clipping/black hole effects
-		offset = 10
-
 		#Operator redo count
-		self.redo+=1
+		self.redo += 1
+
+		#Check georef
+		scn = context.scene
+		geoscn = GeoScene(scn)
+		if not geoscn.isGeoref:
+			self.report({'ERROR'}, "Scene isn't georef")
+			return {'CANCELLED'}
 
 		#Make sure we are in object mode
 		try:
 			bpy.ops.object.mode_set(mode='OBJECT')
 		except:
 			pass
-		bpy.ops.object.select_all(action='DESELECT')
+
+		#Validate selection
+		objs = bpy.context.selected_objects
+		if (not objs or len(objs) > 2) or \
+		(len(objs) == 1 and not objs[0].type == 'MESH') or \
+		(len(objs) == 2 and not set( (objs[0].type, objs[1].type )) == set( ('MESH','CAMERA') ) ):
+			self.report({'ERROR'}, "Pre-selection is incorrect")
+			return {'CANCELLED'}
+
+		#Flag new camera creation
+		if len(objs) == 2:
+			newCam = False
+		else:
+			newCam = True
 
 		#Get georef data
-		geoscn = GeoScene(scn)
 		dx, dy = geoscn.getOriginPrj()
 
-		#Get object
-		objIdx = self.objLst
-		georefObj = scn.objects[int(objIdx)]
+		#Allocate obj
+		for obj in objs:
+			if obj.type == 'MESH':
+				georefObj = obj
+			elif obj.type == 'CAMERA':
+				camObj = obj
+				cam = camObj.data
+				
 
-		#Object properties
-		bbox = BBOX.fromObj(georefObj, True)
+		#Get mesh object properties
+		bbox = BBOX.fromObj(georefObj, applyTransform = True)
 		locx, locy, locz = bbox.center
 		dimx, dimy, dimz = bbox.dimensions
 		#dimx, dimy, dimz = georefObj.dimensions #dimensions property apply object transformations (scale and rot.)
 
-		if self.camLst == 'NEW':
-			#Add camera data
+		#Set active cam
+		if newCam:
 			cam = bpy.data.cameras.new(name=self.name)
-			#Add camera obj
+			cam['mapRes'] = self.target_res #custom prop
 			camObj = bpy.data.objects.new(name=self.name, object_data=cam)
 			scn.objects.link(camObj)
-		else:
-			#Get camera obj
-			objIdx = self.camLst
-			camObj = scn.objects[int(objIdx)]
-			#Get data
-			cam = camObj.data
+			scn.camera = camObj			
+		elif self.redo == 1: #first exec, get initial camera res
+			scn.camera = camObj
+			try:
+				self.target_res = cam['mapRes']
+			except:
+				self.report({'ERROR'}, "This camera has not map resolution property")
+				return {'CANCELLED'}	
+		else: #following exec, set camera res in redo panel
+			try:
+				cam['mapRes'] = self.target_res
+			except:
+				self.report({'ERROR'}, "This camera has not map resolution property")
+				return {'CANCELLED'}			
 
 		#Set camera data
 		cam.type = 'ORTHO'
 		cam.ortho_scale = max((dimx, dimy)) #ratio = max((dimx, dimy)) / min((dimx, dimy))
+
+		#General offset used to set cam z loc and clip end distance
+		#needed to avoid clipping/black hole effects
+		offset = dimz * 50/100
 
 		#Set camera location
 		camLocZ = bbox['zmin'] + dimz + offset
@@ -146,13 +140,15 @@ class SetGeorenderCam(bpy.types.Operator):
 		cam.clip_end = dimz + offset*2
 		cam.show_limits = True
 
-		if self.camLst != 'NEW':
-			if self.redo == 1:#in update mode, we don't want overwrite initial camera name
+		if not newCam:
+			if self.redo == 1:#first exec, get initial camera name
 				self.name = camObj.name
-			else:#but user can change camera name in redo parameters
+			else:#following exec, set camera name in redo panel
 				camObj.name = self.name
 				camObj.data.name = self.name
 
+		#Update selection
+		bpy.ops.object.select_all(action='DESELECT')
 		camObj.select = True
 		scn.objects.active = camObj
 
@@ -162,19 +158,24 @@ class SetGeorenderCam(bpy.types.Operator):
 		scn.render.resolution_y = dimy / self.target_res
 		scn.render.resolution_percentage = 100
 
-		#write wf
+		#Write wf
 		res = self.target_res#dimx / scene.render.resolution_x
-		rot=0
+		rot = 0
 		x = bbox['xmin'] + dx
 		y = bbox['ymax'] + dy
 		wf_data = str(res)+'\n'+str(rot)+'\n'+str(rot)+'\n'+str(-res)+'\n'+str(x+res/2)+'\n'+str(y-res/2)
-		wf_name = self.name+'.wld'
+		wf_name = camObj.name + '.wld'
 		if wf_name in bpy.data.texts:
 			wfText = bpy.data.texts[wf_name]
 			wfText.clear()
 		else:
 			wfText = bpy.data.texts.new(name=wf_name)
 		wfText.write(wf_data)
-
+		
+		#Purge old wf text
+		for wfText in bpy.data.texts:
+			name, ext = wfText.name[:-4], wfText.name[-4:]
+			if ext == '.wld' and name not in bpy.data.objects:
+				bpy.data.texts.remove(wfText)
 
 		return {'FINISHED'}
