7fb8a66c4a487639306acad20508beceb44fadbb
domlysz <domlysz@gmail.com>
2025-12-17 17:09:56 +0100

    bump version

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ bl_info = {
 	'author': 'domlysz',
 	'license': 'GPL',
 	'deps': '',
-	'version': (2, 2, 13),
+	'version': (2, 2, 14),
 	'blender': (2, 83, 0),
 	'location': 'View3D > Tools > GIS',
 	'warning': '',
